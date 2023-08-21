9722df23a47963d425dc0b09d42c4c09c92bdb0e
X1atle <myron.ryzleitsev@gmail.com>
2023-08-21 18:51:11 +0200

    XAML Design
    The provided XAML code represents the design of the main window for a Tic-Tac-Toe game. The layout incorporates various UI elements to create a functional game interface.

--- Tic-Tac-Toe/MainWindow.xaml.cs
+++ Tic-Tac-Toe/MainWindow.xaml.cs
@@ -15,14 +15,22 @@ using System.Windows.Shapes;
 
 namespace Tic_Tac_Toe
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void GameGrid_MoseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
