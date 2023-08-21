d7f5b1abd8223e7d70727d52a06c4ec64c77dcfb
X1atle <myron.ryzleitsev@gmail.com>
2023-08-22 00:21:10 +0200

    Updated Event Handling and UI Interaction
    Enhanced UI Interaction: Player moves now update the game board with corresponding symbols, and the current player's turn is visually indicated. Improved handling of user interactions and state updates for the Tic-Tac-Toe game window

--- Tic-Tac-Toe/MainWindow.xaml.cs
+++ Tic-Tac-Toe/MainWindow.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Mime;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Tic_Tac_Toe
 {
@@ -21,10 +14,10 @@ namespace Tic_Tac_Toe
 
         private readonly Dictionary<Player, ImageSource> imageSources = new()
         {
-            { Player.X, new BitmapImage(new Uri("pack:/application:,,,/Assets/X15.png")) },
-            { Player.O, new BitmapImage(new Uri("pack:/application:,,,/Assets/O15.png")) }
+            { Player.X, new BitmapImage(new Uri("pack://application:,,,/Assets/X15.png")) },
+            { Player.O, new BitmapImage(new Uri("pack://application:,,,/Assets/O15.png")) }
         };
-
+        
         private readonly Image[,] imageControls = new Image[3, 3];
         private readonly GameState gameState = new GameState();
 
@@ -53,7 +46,9 @@ namespace Tic_Tac_Toe
 
         private void OnMoveMade(int r, int c)
         {
-            
+            Player player = gameState.GameGrid[r, c];
+            imageControls[r, c].Source = imageSources[player];
+            PlayerImage.Source = imageSources[gameState.CurrentPlayer];
         }
 
         private void OnGameEnded(GameResult gameResult)
@@ -68,7 +63,11 @@ namespace Tic_Tac_Toe
 
         private void GameGrid_MoseDown(object sender, MouseButtonEventArgs e)
         {
-            
+            double squareSize = GameGrid.Width / 3;
+            Point clickPosition = e.GetPosition(GameGrid);
+            int row = (int)(clickPosition.Y / squareSize);
+            int col = (int)(clickPosition.X / squareSize);
+            gameState.MakeMove(row, col);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
