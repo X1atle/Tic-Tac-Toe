e39da206ee0efc67864a7b72b3187c9ac501e243
X1atle <myron.ryzleitsev@gmail.com>
2023-08-22 21:47:10 +0200

    Enhanced Animations and UI Refinements
    Highlights include:
    
        Enhanced Line Animation:
            The ShowLine method now includes smoother animations for drawing the winning line.
            Two separate animations for X2 and Y2 properties of the line create a more realistic drawing effect.
            A short delay after the animation completes allows the line to fully appear before proceeding.
    
        Refined Click Event Handling:
            The Button_Click event handling logic has been updated to ensure that the game can only be restarted when it's truly over (GameOver is true).
            This prevents accidental restarts during transitions and maintains a consistent user experience.
    
        Code Organization and Cleanup:
            The code continues to be well-organized and commented for clarity.
            Redundant code has been minimized, and unnecessary parts have been removed.
    
    With these enhancements, the game offers smoother and more immersive animations, as well as improved interactivity during transitions.

--- Tic-Tac-Toe/MainWindow.xaml.cs
+++ Tic-Tac-Toe/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 
 namespace Tic_Tac_Toe
@@ -139,17 +140,31 @@ namespace Tic_Tac_Toe
             return (new Point(GameGrid.Width, 0), new Point(0, GameGrid.Height));
         }
 
-        private void ShowLine(WinInfo winInfo)
+        private async Task ShowLine(WinInfo winInfo)
         {
             (Point start, Point end) = FindLinePoints(winInfo);
 
             Line.X1 = start.X;
             Line.Y1 = start.Y;
 
-            Line.X2 = end.X;
-            Line.Y2 = end.Y;
+            DoubleAnimation x2Animation = new DoubleAnimation()
+            {
+                Duration = TimeSpan.FromSeconds(.25),
+                From = start.X,
+                To = end.X
+            };
+            
+            DoubleAnimation y2Animation = new DoubleAnimation()
+            {
+                Duration = TimeSpan.FromSeconds(.25),
+                From = start.Y,
+                To = end.Y
+            };
 
             Line.Visibility = Visibility.Visible;
+            Line.BeginAnimation(Line.X2Property, x2Animation);
+            Line.BeginAnimation(Line.Y2Property, y2Animation);
+            await Task.Delay(x2Animation.Duration.TimeSpan);
         }
         
         private void OnMoveMade(int r, int c)
@@ -168,7 +183,7 @@ namespace Tic_Tac_Toe
             }
             else
             {
-                ShowLine(gameResult.WinInfo);
+                await ShowLine(gameResult.WinInfo);
                 await Task.Delay(1000);
                 await TransistionToEndScreen("Winner:", imageSources[gameResult.Winner]);
             }
